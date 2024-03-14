4be7a4c37b9a0d8f3ae222edbb3bb6780d9a96a7
İpek <zekiyeipek_ozmen@hotmail.com>
2024-03-15 01:23:08 +0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/C05n9nPV1GzJOSV3/scene.splinecode" />
+  );
+}
